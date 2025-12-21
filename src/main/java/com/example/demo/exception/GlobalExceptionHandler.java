e71d718919ca806823668927300021a82e855b79
bala-amypo <bala@amypo.in>
2025-12-21 06:09:45 +0000

    User commit

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,5 +1,13 @@
 package com.example.demo.exception;
 
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
-    
+
+  @ExceptionHandler(ResourceNotFoundException.class)
+  public String handle(ResourceNotFoundException ex) {
+    return ex.getMessage();
+  }
 }
