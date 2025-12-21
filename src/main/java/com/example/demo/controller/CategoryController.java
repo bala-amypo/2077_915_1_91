3961e699b99b361618dbc20f2255fc4fe58e104f
bala-amypo <bala@amypo.in>
2025-12-21 13:31:03 +0000

    User commit

--- src/main/java/com/example/demo/controller/CategoryController.java
+++ src/main/java/com/example/demo/controller/CategoryController.java
@@ -1,8 +1,13 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CategoryController {
-    
+
+    @GetMapping("/health")
+    public String health() {
+        return "Application is running successfully";
+    }
 }
