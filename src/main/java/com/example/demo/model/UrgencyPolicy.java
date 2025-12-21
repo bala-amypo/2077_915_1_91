9f3d82e040a636743fcc136c90db520671aacf6e
bala-amypo <bala@amypo.in>
2025-12-21 07:57:53 +0000

    User commit

--- src/main/java/com/example/demo/model/UrgencyPolicy.java
+++ src/main/java/com/example/demo/model/UrgencyPolicy.java
@@ -1,44 +1,26 @@
 package com.example.demo.model;
 
 import jakarta.persistence.*;
-import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.Set;
 
 @Entity
-@Table(name = "urgency_policies")
 public class UrgencyPolicy {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String policyName;
-
     private String keyword;
 
     private String urgencyOverride;
 
-    private LocalDateTime createdAt;
-
-    @ManyToMany(mappedBy = "urgencyPolicies")
-    private Set<Category> categories = new HashSet<>();
-
-    @PrePersist
-    public void prePersist() {
-        this.createdAt = LocalDateTime.now();
-    }
-
+ 
     public Long getId() {
         return id;
     }
 
-    public String getPolicyName() {
-        return policyName;
-    }
 
-    public void setPolicyName(String policyName) {
-        this.policyName = policyName;
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getKeyword() {
@@ -55,13 +37,5 @@ public class UrgencyPolicy {
 
     public void setUrgencyOverride(String urgencyOverride) {
         this.urgencyOverride = urgencyOverride;
-    }
-
-    public Set<Category> getCategories() {
-        return categories;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
     }
 }
