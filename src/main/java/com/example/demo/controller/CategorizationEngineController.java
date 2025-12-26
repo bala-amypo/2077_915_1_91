d5d9188fa400d227f802da39e71e5445d2bbdfcc
bala-amypo <bala@amypo.in>
2025-12-26 13:46:52 +0000

    User commit

--- src/main/java/com/example/demo/controller/CategorizationEngineController.java
+++ src/main/java/com/example/demo/controller/CategorizationEngineController.java
@@ -3,7 +3,12 @@ package com.example.demo.controller;
 import com.example.demo.model.CategorizationLog;
 import com.example.demo.model.Ticket;
 import com.example.demo.service.CategorizationEngineService;
-import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -18,17 +23,17 @@ public class CategorizationEngineController {
     }
 
     @PostMapping("/run/{ticketId}")
-    public Ticket categorizeTicket(@PathVariable Long ticketId) {
+    public Ticket categorizeTicket(@PathVariable("ticketId") Long ticketId) {
         return engineService.categorizeTicket(ticketId);
     }
 
     @GetMapping("/logs/{ticketId}")
-    public List<CategorizationLog> getLogsForTicket(@PathVariable Long ticketId) {
+    public List<CategorizationLog> getLogsForTicket(@PathVariable("ticketId") Long ticketId) {
         return engineService.getLogsForTicket(ticketId);
     }
 
     @GetMapping("/log/{id}")
-    public CategorizationLog getLog(@PathVariable Long id) {
+    public CategorizationLog getLog(@PathVariable("id") Long id) {
         return engineService.getLog(id);
     }
 }
