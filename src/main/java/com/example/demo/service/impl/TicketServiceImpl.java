79a51d8b49fc7278c35d6932a0ec785b78760248
bala-amypo <bala@amypo.in>
2025-12-27 06:07:12 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/TicketServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/TicketServiceImpl.java
@@ -1,10 +1,12 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.dto.CreateTicketRequest;
 import com.example.demo.model.Ticket;
 import com.example.demo.repository.TicketRepository;
 import com.example.demo.service.TicketService;
+import com.example.demo.exception.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TicketServiceImpl implements TicketService {
@@ -16,12 +18,18 @@ public class TicketServiceImpl implements TicketService {
     }
 
     @Override
-    public Ticket createTicket(CreateTicketRequest request) {
-
-        Ticket ticket = new Ticket();
-        ticket.setTitle(request.getTitle());
-        ticket.setDescription(request.getDescription());
-
+    public Ticket createTicket(Ticket ticket) {
         return ticketRepository.save(ticket);
+    }
+
+    @Override
+    public Ticket getTicket(Long id) {
+        return ticketRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Ticket not found"));
+    }
+
+    @Override
+    public List<Ticket> getAllTickets() {
+        return ticketRepository.findAll();
     }
 }
