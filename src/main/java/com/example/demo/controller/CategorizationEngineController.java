024bfaf52ee1d7ee77d0ae37049986bfe3aaf671
bala-amypo <bala@amypo.in>
2025-12-28 11:20:10 +0000

    User commit

--- src/main/java/com/example/demo/controller/CategorizationEngineController.java
+++ src/main/java/com/example/demo/controller/CategorizationEngineController.java
@@ -1,15 +1,4 @@
 package com.example.demo.controller;
 
-import com.example.demo.service.CategorizationEngineService;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("/engine")
 public class CategorizationEngineController {
-
-    private final CategorizationEngineService service;
-
-    public CategorizationEngineController(CategorizationEngineService service) {
-        this.service = service;
-    }
 }
--- src/main/java/com/example/demo/controller/UrgencyPolicyController.java
+++ src/main/java/com/example/demo/controller/UrgencyPolicyController.java
@@ -1,15 +1,4 @@
 package com.example.demo.controller;
 
-import com.example.demo.service.UrgencyPolicyService;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("/policies")
 public class UrgencyPolicyController {
-
-    private final UrgencyPolicyService service;
-
-    public UrgencyPolicyController(UrgencyPolicyService service) {
-        this.service = service;
-    }
 }
