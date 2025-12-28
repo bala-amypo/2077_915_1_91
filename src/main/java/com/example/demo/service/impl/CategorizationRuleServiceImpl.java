2685b9f2803149f5406c646ae17019e746cd124e
bala-amypo <bala@amypo.in>
2025-12-28 09:20:23 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CategorizationRuleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CategorizationRuleServiceImpl.java
@@ -3,24 +3,30 @@ package com.example.demo.service.impl;
 import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.CategorizationRule;
 import com.example.demo.repository.CategorizationRuleRepository;
+import com.example.demo.repository.CategoryRepository;
 import com.example.demo.service.CategorizationRuleService;
 
 public class CategorizationRuleServiceImpl implements CategorizationRuleService {
 
-    private final CategorizationRuleRepository repository;
+    private final CategorizationRuleRepository ruleRepository;
+    private final CategoryRepository categoryRepository;
 
-    public CategorizationRuleServiceImpl(CategorizationRuleRepository repository) {
-        this.repository = repository;
+    public CategorizationRuleServiceImpl(
+            CategorizationRuleRepository ruleRepository,
+            CategoryRepository categoryRepository
+    ) {
+        this.ruleRepository = ruleRepository;
+        this.categoryRepository = categoryRepository;
     }
 
     @Override
     public CategorizationRule createRule(CategorizationRule rule) {
-        return repository.save(rule);
+        return ruleRepository.save(rule);
     }
 
     @Override
     public CategorizationRule getRule(Long id) {
-        return repository.findById(id)
+        return ruleRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Rule not found"));
     }
 }
